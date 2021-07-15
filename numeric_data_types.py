2dea564b803c7942c58e5ea8cb21b5e4d2b07e84
Peter Chung <85366746+PeterJChung@users.noreply.github.com>
2021-07-15 14:49:31 -0400

    Create numeric_data_types.py
    Lab 109 - Numeric data types

---
+++ numeric_data_types.py
@@ -0,0 +1,6 @@
+# Numeric Data Types
+print("Python has 3 numeric types: int, float, complex")
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of data type " + str(type(myValue)))
